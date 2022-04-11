90d086c7910b3921e85c56a7aef8f1d323ec89e3
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2022-04-11 21:24:10 +1200

    added more comments to make my code more intelligible

--- main.py
+++ main.py
@@ -1,239 +1,288 @@
-if __name__ == '__main__':
-    '''
-    decomposition
-    1) Ask the user for their name and set this to a variable so that we can refer to them later in the program
-    2) Create a function that presents users with a question and allows them to pick an answer from multiple choices
-    A. create another function(or maybe a class) to check whether a user clicks within a buttons hitbox
-    B. darken questions that are clicked by the user
-    C. allow this function to takein a background to also display to the user
-    3)create multiple maths question
-    A. make these maths questions vary in difficulty
-    B. make the questions harder the more they succeed
-    4) After completing the quiz display to users their mistakes and overall percentage on the test
-    a. record what questions they got wrong
-    b) find a percentage using the total questions divided by the number of questions they got right they got right
-    5) Ask the user whether they want to try again(show improvements over time)
-    '''
+import sys
 
-    # imports
-    import sys
-    import random
-    import pygame
-    import time
-    from pygame.locals import *
+import random
+import pygame
+import math
+from pygame.locals import *
 
-    pygame.init()  # starts pygame
+pygame.init()
 
-    # frames per second setup
-    fps = 60
-    fpsClock = pygame.time.Clock()
-    size = 1 # this variable scales all the screen sizes and buttons
-    # screen setup
+fps = 60
+fpsClock = pygame.time.Clock()
 
-    def init():
+width, height = 1000, 480
 
-        global width, height, screen, font_size, font, font2, button_size_x, button_size_y, button_distance
-        width, height = 900 * size, 480 * size
-        screen = pygame.display.set_mode((width, height))
-        font_size = round(30 * size)
-
-        font = pygame.font.SysFont('Arial', font_size) # font for the title
-        font2 = pygame.font.SysFont('Arial', round(30*size)) # font for the title
-
-        button_size_x = 400 * size
-        button_size_y = 150 * size
-        button_distance = 460 * size
-
-    init()
-
-    # constants and colours
-
-    # variables, dictionaries and lists
-    question = 1
-    score = 0
-    question_dictionary = {1: ["what is 1+1", "2", "window", "11", "IDK",], #question dictionary holds all the questions and answer
-                           2: ["5 + b = 12,  what is the value of b", "7", "2", "5", "what?"],
-                           3: ["there were 5 birds, 2 flew away. how many birds are left?", "3", "1000000000", "4", "whats a bird"],
-                           4: ["10 x 4 = 40", "True", "False"],
-                           5: ["20 - 10 = 5", "False", "True"],
-                           6: ["3 + 8 x 3", "27", "33"],
-                           7: ["what is 8 x 8", "64", "16", "88", "42"],
-                           8: ["what is 12 ÷ 3", "4", "2", "3", "9"],
-                           9: ["9 x 9", "81", "36", "72", "18"],
-                           10: ["a square has a width of 5, what is the area of the square", "25", "not enough infomation", "5", "20"],
-                           11: ["8-4*8", "-24", "24"],
-                           12: ["9 x 6 = 54", "True", "False"],
-                           13: ["16 / 4", "4", "3", "2", "8"],
-                           14: ["8+8+8+8+8", "40", "32", "48", "8"],
-                           15: ["what is 12 x 8", "96", "48", "112", "92"],
-                           16: ["20 x 20", "400", "4400", "40", "440"],
-                           17: ["what is 9 x 5 x 2?", "90", "952"],
-                           18: ["You scored      %,", "True", "False"]
+screen = pygame.display.set_mode((width, height))
+top_screen = pygame.Surface((width, height/2))
 
 
-                           }
 
 
-    # print(question_dictionary.get((question))[0]) TEST
 
-    class button:
-        def __init__(self, x, y, text):
-
-            self.correct = False
-            self.x = x
-            self.y = y
-            self.text = text
+global_x = 0
+global_y = 0
 
 
-        def display(self, colour):
+resolution = 3
+#colours
+RED = (255, 0, 0)
+class player:
+    def __init__(self):
+        self.speed = 10
+        self.x = width/2
+        self.y = height/2
+        self.fov = 80
+        self.direction = 0
+p1 = player()
+class object:
+    def __init__(self, x, y, size_x, size_y):
+        self.x = x
+        self.y = y
+        self.size_x = size_x
+        self.size_y = size_y
+        self.colour = (100, 100, 100)
 
-            pygame.draw.rect(screen, colour, pygame.Rect(self.x, self.y, button_size_x, button_size_y)) #draws button onto the screen
-            text = font2.render(self.text, False, (0, 0, 0)) #sets up text surface
-            text_width = text.get_width()  # gets text width
-            text_height = text.get_height()
-            screen.blit(text, (self.x+button_size_x/2-text_width/2, self.y+button_size_y/2-text_height/2)) #displays text surface perfectly centred using the text width
-    def get_statistics(score):
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, self.size_x, self.size_y)
+    def display(self):
+        pygame.draw.rect(screen, self.colour, pygame.Rect(self.x + global_x, self.y + global_y, self.size_x, self.size_y))
+class ray:
+    def __init__(self, x, y, x_vel, y_vel):
+        self.x = x
+        self.y = y
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.distance_x = 0
+        self.distance_y = 0
+        self.distance = 0
 
-        if int(score) > 70:
-            score_text = font.render(score, False, (0, 255, 0))
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, 10*resolution/5, 10*resolution/5)
+
+    def display(self):
+        pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(self.x + global_x, self.y + global_y, 10*resolution/5, 10*resolution/5))
+
+
+
+
+def create_level():
+    global objects
+    objects = []
+
+    for x in range(5):
+        objects.append(object(random.randint(0, 1000), random.randint(0, 1000), random.randint(100, 200), random.randint(100, 200)))
+
+    for obj in objects.copy():
+        if obj.x > p1.x + 100 or obj.x < p1.x - 300 or obj.y > p1.y + 100 or obj.y < p1.y - 280:
+            pass
         else:
-            score_text = font.render(score, False, (255, 0, 0))
-        display_question("None", question)
-        screen.blit(score_text, (485*size, 40*size))
-        pygame.display.update()
-
-        time.sleep(6)
-
-        pygame.quit() #quits pygame
-        exit() #ends program
+            objects.remove(obj)
 
 
+    objects.append(object(0, 0, 1000, 100))
+    objects.append(object(0, 0, 100, 1000))
+    objects.append(object(0, 1000, 1100, 100))
+    objects.append(object(1000, 0, 100, 1000))
+
+def check_hitboxes(placex, placey):
+
+    for obj in objects:
+        if placex*-1 > obj.x - p1.x and placex*-1 < obj.x  + obj.size_x - p1.x and placey*-1 > obj.y - p1.y and placey*-1 < obj.y  + obj.size_y - p1.y:
+
+            return False
+    return True
 
 
-    def check_mouse_inputs(x, y):
-        global question
-        global score
-
-        for button in buttons:
-            if button.x+button_size_x > x and button.x < x and button.y+button_size_y > y and button.y < y: #clicking on the button
-                if button.correct: #if the button has the attribute "Correct = True" has been clicked then the user has inputed the right answer
-                    button.display((0, 255, 0))
-                    score += 1
-
-                elif button.correct == False:
-                    button.display((255, 0, 0))
-
-                    for button in buttons:
-                        if button.correct:  # if the button has the attribute "Correct = True" has been clicked then the user has inputed the right answer
-                            button.display((0, 255, 0))
-                    pygame.display.flip()
-                    pygame.time.wait(2000)
-
-                pygame.display.flip() #updates the screen
-                pygame.time.wait(500)
-
-                question += 1 #goes to the next question
-
-                randomise_question(question) #randomisises question answers
-                return True
-    def randomise_question(question):
-        global buttons
-        buttons = []
-        if question < len(question_dictionary):
-
-            if len(question_dictionary.get(question)) == 3:  #number of given answers = 2 (true/false question)
-                values = [1, 2]
-                for x in range(2):
-                    choice = random.choice(values)
-                    buttons.append(button(20 * size+button_distance*x, height - 270 * size, question_dictionary.get(question)[choice]))   # button1
-                    values.remove(choice)
-
-
-            if len(question_dictionary.get(question)) == 5:
-                #creates 4 buttons from the button class
-                values = [1, 2, 3, 4]
-
-                for x in range(2):
-                    choice = random.choice(values)
-                    buttons.append(button(20 * size + button_distance*x, height - 330 * size, question_dictionary.get(question)[choice]))   # button1
-                    values.remove(choice)
-
-                for x in range(2):
-                    choice = random.choice(values)
-                    buttons.append(button(20 * size + button_distance*x, height - 160 * size, question_dictionary.get(question)[choice]))
-                    values.remove(choice)
-
-            for x in buttons:
-                if x.text == question_dictionary.get(question)[1]:
-                    x.correct = True
-        else:
-            get_statistics(str(round(score / (len(question_dictionary) - 1) * 100)))
-
-
-    def display_question(background, question_number):
-        global buttons
-
-        # screen.blit(background)
-        if background == "None":
-            screen.fill((0, 0, 255))
-        else:
-            screen.blit(background, 0, 0)
-
-        text_1 = str(question_number) #i'll fix this messy code later using .join() function
-        text_2 = "/"
-        text_3 = str(len(question_dictionary)-1)
-
-
-        question = question_dictionary.get(question_number)[0]  # finds a question stored in the dictionary
-
-        pygame.draw.ellipse(screen, (247, 95, 7), pygame.Rect(50*size, 20*size, width-100*size, 100 * size)) #draws eclipse around the question
-
-        pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(0, 0, 80 * size, 40 * size)) #draws a rectangle around the question counter for visibility
-
-
-        question_title = font.render(str(question), False, (255, 255, 255)) #creates a surface for the question to go on
-        question_number_text_1 = font.render(text_1, False, (0, 0, 0))
-        question_number_text_2 = font.render(text_2, False, (0, 0, 0))
-        question_number_text_3 = font.render(text_3, False, (0, 0, 0))
-
-        text_width = question_title.get_width() # finds the width of the text surface
-        screen.blit(question_title, ((width / 2) - (text_width / 2), 40 * size)) #displays the question in the middle of the screen
-
-        screen.blit(question_number_text_1, (1*size, -1*size))
-        screen.blit(question_number_text_2, (25*size, -1*size))
-        screen.blit(question_number_text_3, (45*size, -1*size))
-
-        #render buttons
-        for button in buttons:
-            button.display((255, 255, 255))
+def shoot_ray(direction, direct):
+    global global_x
+    global global_y
+    distance_x = 0
+    distance_y = 0
 
 
 
-    randomise_question(question)
-    # Game loop.
-    while True:
+
+    direction -= 90
+    direction = direction * math.pi/180
+    r1 = ray(global_x * -1 + p1.x, global_y * -1 + p1.y, math.cos(direction) * 4*resolution, math.sin(direction) * 4*resolution)
+
+    moved = True
+    while moved == True:
+
+        for obj in objects:
+            if r1.get_rect().colliderect(obj.get_rect()):
+                moved = False
+                while r1.get_rect().colliderect(obj.get_rect()):
+                    r1.x -= r1.x_vel * 0.01
+                    r1.y -= r1.y_vel * 0.01
+                    r1.distance_x -= r1.x_vel * 0.01
+                    r1.distance_y -= r1.y_vel * 0.01
+                r1.display()
+        if moved == True:
+            r1.x += r1.x_vel
+            r1.y += r1.y_vel
+            r1.distance_x += r1.x_vel
+            r1.distance_y += r1.y_vel
+    r1.distance = math.sqrt((r1.distance_x ** 2 + r1.distance_y ** 2))
+    #r1.distance = r1.distance * math.cos(direction)
+    rays.append(r1)
+
+
+
+
+
+
+def get_input():
+    global global_x
+    global global_y
+
+
+    keys_pressed = pygame.key.get_pressed()  # Gets all pressed keys
+
+    direction = p1.direction * (math.pi/180)
+    direction -= 180
+    x_vel = math.cos(direction) * p1.speed
+    y_vel = math.sin(direction) * p1.speed
+
+
+    if keys_pressed[pygame.K_a] and keys_pressed[
+        pygame.K_w]:  # these check if you are trying to move diagonally, and if so reduce your speed to normal (speed/1.41)
+        global_x += p1.speed / 1.41
+        global_y += p1.speed / 1.41
+        if check_hitboxes(global_x, global_y) == False:
+            global_x -= p1.speed / 1.41
+            global_y -= p1.speed / 1.41
+    elif keys_pressed[pygame.K_a] and keys_pressed[pygame.K_s]:  # A+S
+        global_x += p1.speed / 1.41
+        global_y -= p1.speed / 1.41
+        if check_hitboxes(global_x, global_y) == False:
+            global_x -= p1.speed / 1.41
+            global_y += p1.speed / 1.41
+    elif keys_pressed[pygame.K_w] and keys_pressed[pygame.K_d]:  # W+D
+        global_x -= p1.speed / 1.41
+        global_y += p1.speed / 1.41
+        if check_hitboxes(global_x, global_y) == False:
+            global_x += p1.speed / 1.41
+            global_y -= p1.speed / 1.41
+    elif keys_pressed[pygame.K_d] and keys_pressed[pygame.K_s]:  # D+S
+        global_x -= p1.speed / 1.41
+        global_y -= p1.speed / 1.41
+        if check_hitboxes(global_x, global_y) == False:
+            global_x += p1.speed / 1.41
+            global_y += p1.speed / 1.41
+
+
+    else:
+        if keys_pressed[pygame.K_a]:  # movement for only one key pressed
+            pass  # Move left
+            if check_hitboxes(global_x, global_y) == False:
+                pass
+
+        if keys_pressed[pygame.K_d]:
+
+            pass
+
+
+            if check_hitboxes(global_x, global_y) == False:
+                pass
+
+        if keys_pressed[pygame.K_w]:
+            global_y += y_vel
+            global_x += x_vel
+
+            if check_hitboxes(global_x, global_y) == False:
+                global_y -= y_vel
+                global_x -= x_vel
+
+
+
+        if keys_pressed[pygame.K_s]:
+            global_y -= y_vel
+            global_x -= x_vel
+            if check_hitboxes(global_x, global_y) == False:
+                global_y += y_vel
+                global_x += x_vel
+    if keys_pressed[pygame.K_LEFT]:
+        p1.direction -= 4
+    if keys_pressed[pygame.K_RIGHT]:
+        p1.direction += 4
+
+def render_3d_world():
+
+    global top_screen
+
+
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, height/2, width, 150))
+
+
+    for x in range(255):
+        pygame.draw.rect(screen, (x, x, x), pygame.Rect(0, height/2+80+x, width, 10))
+
+
+
+
+    for i, ray in enumerate(rays):
+        colour = 255 - ray.distance*0.6
+        if colour < 0:
+            colour = 0
+        if colour > 255:
+            colour = 255
+        pygame.draw.rect(screen, (colour, colour, colour), pygame.Rect(((width/len(rays))*i), height/2, 12, height-ray.distance))
+        pygame.draw.rect(top_screen, (colour, colour, colour), pygame.Rect(((width / len(rays))*i), 0, 12, ray.distance))
+
+    top_screen = pygame.transform.flip(top_screen, False, True)
+    for x in range(255):
+        pygame.draw.rect(top_screen, (abs(x-255), abs(x-255), abs(x-255)), pygame.Rect(0, x/4, width, x/4))
+    screen.blit(top_screen, (0, 0))
+
+
+counter = 0
+
+def display():
+    global rays
+    global direct
+    global counter
+    pygame.draw.rect(screen, RED, pygame.Rect(p1.x, p1.y, 5, 5))
+
+
+
+    for object in objects:
+
+
+        object.display()
+    #pygame.draw.polygon(screen, (255, 0, 0),
+                        #points=[(p1.x, p1.y), (p1.direction + 360, 100), (p1.direction + 360 + p1.fov, 100)])
+    rays = []
+    total_rays = (round(p1.fov/resolution*5))
+    print(total_rays)
+    for i in range(total_rays):
+        #spacing = ((i - (round(total_rays/2)))) * 0.1
+        spacing = 0
+        shoot_ray(p1.direction + i*resolution/5-spacing,0)
+
+    render_3d_world()
+    counter += 1
+    if counter > fps*2-30 and counter < fps*2:
         screen.fill((0, 0, 0))
+        counter = 0
 
 
-        display_question("None", question)
 
+create_level()
+# Game loop.
+while True:
+    screen.fill((0, 0, 0))
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                check_mouse_inputs(x, y)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_EQUALS or event.key == pygame.K_MINUS:
-                    if event.key == pygame.K_EQUALS and size < 3:
-                        size += 0.1
-                    if event.key == pygame.K_MINUS and size > 0.2:
-                        size -= 0.1
-                    init()
-                    randomise_question(question)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    get_input()
 
+    display()
+    # Update.
 
-        pygame.display.flip()
-        fpsClock.tick(fps)
+    # Draw.
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
